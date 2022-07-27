7182cfd58d5d5c0ac93f8c71ebdf29398df83994
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-07-27 11:45:47 -0500

    update test page

--- pages/onDemand/test.js
+++ pages/onDemand/test.js
@@ -8,7 +8,7 @@ const LoadDataViaApi = (props) => {
       method: "POST",
       mode: 'no-cors',
       headers: {
-        'Authorization': `Bearer 3cb6d9f6-d011-49fc-858f-c4c1127083d7`,
+        'Authorization': `Bearer ${process.env.API_KEY_FULL_CORS}`,
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         // 'Access-Control-Allow-Credentials': 'true',
