628b71fad22983f675fe56331ac5c8a2900b4401
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-07-30 19:23:45 -0500

    update get asset by id page

--- pages/api/getAsset.js
+++ pages/api/getAsset.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function handler(req, res) {
-  const { assetId } = req.body;
+  const { assetId } = req.body.assetId;
   try {
     const response = await fetch(`https://livepeer.studio/api/asset/${assetId}`, {
       method: "GET",
@@ -8,16 +8,11 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.API_KEY}`,
         'Content-Type': 'application/json',
       },
-      // body: JSON.stringify({
-      //   name,
-      //   url
-      // })
     })
    
     const data = await response.json();
     console.log(data);
-    // res.status(200).json(data)
-    // need to redirect to the upload page again
+   
     res.status(200).json(data);
   } catch (error  ) {
     console.log(error)
