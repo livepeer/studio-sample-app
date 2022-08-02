43d945ca1c9f4b363c92ba62c58f86064d849e12
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-08-02 07:59:46 -0500

    replace PUT with PATCH

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,26 +1,25 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function handler(req, res) {
-  const { name, storage, meta } = req.body;
-  const assetId = req.body.assetId;
+  const { assetId, name, storage, meta } = req.body;
+  console.log(req.body);
   try {
     const response = await fetch(`https://livepeer.studio/api/asset/${assetId}`, {
-      method: "PUT",
+      method: "PATCH",
       headers: {
-        'Authorization': `Bearer ${process.env.API_KEY}`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.API_KEY}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name,
         storage,
         meta
-      })
-    })
-   
+      }),
+    });
+
     const data = await response.json();
     console.log(data);
-    res.status(200).json(data)
+    res.status(200).json(data);
   } catch (error) {
-    console.log(message.error)
+    res.status(400).send("error");
   }
-  res.status(400).send("error")
 }
