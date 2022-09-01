13bbd8fbc23f5129825734b4b949cc8a68e86b49
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-09-01 14:26:04 -0500

    add CID info to asset and style

--- pages/videoAssets/[id].js
+++ pages/videoAssets/[id].js
@@ -1,17 +1,16 @@
-import Image from "next/image";
-import { useRouter } from "next/router";
-import logo from "../../public/studioLogo.png";
-import styles from "../../styles/Asset.module.css";
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import logo from '../../public/studioLogo.png';
+import styles from '../../styles/Asset.module.css';
 
-
-// Calling the api from server side using 'getServerSideProps' and passing in existing 
+// Calling the api from server side using 'getServerSideProps' and passing in existing
 // routes from 'getStaticPaths' for dynamic routing
 export async function getServerSideProps({ params }) {
   const res = await fetch(`https://livepeer.studio/api/asset/${params.id}`, {
-    method: "GET",
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${process.env.API_KEY}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
   // Convert json response into JS object
@@ -24,7 +23,7 @@ export async function getServerSideProps({ params }) {
   };
 }
 
-// Function to display each asset with their own information 
+// Function to display each asset with their own information
 export default function AssetDetails({ assets }) {
   // Accessing the 'query' from the router object to passing in the id of each asset for dynamic routing
   const {
@@ -34,45 +33,67 @@ export default function AssetDetails({ assets }) {
   return (
     <div>
       <div className={styles.card} key={id}>
-        {assets.status.phase === "ready" ? (
+        {assets.status.phase === 'ready' ? (
           <div>
             {/* Display embedded Video Player if it exists, otherwise show an image */}
             <iframe
               className={styles.iframe}
               src={`https://lvpr.tv?v=${assets.playbackId}`}
-              frameForder="0"
-              height="300px"
+              frameForder='0'
+              height='300px'
               allowFullScreen
-              allow="autoplay; encrypted-media; picture-in-picture"
-              sandbox="allow-scripts"
+              allow='autoplay; encrypted-media; picture-in-picture'
+              sandbox='allow-scripts'
             ></iframe>
 
             {/* Code for embedding the video */}
             <div className={styles.embedInfo}>
-              <code>
-                <p>Embed Player Code</p>
-                <br />
-                {`<iframe>`}
-                <br />
-                src={`https://lvpr.tv?v=${assets.playbackId}`}&apos;
-                <br />
-                allow=&apos;autoplay; encrypted-media; picture-in-picture&apos;
-                <br />
-                sandbox=&apos;allow-scripts&apos;
-                <br />
-                {`</iframe>`}
-              </code>
+              <div>
+                <code>
+                  <p>Embed Player Code w/ PlaybackId</p>
+                  <br />
+                  {`<iframe>`}
+                  <br />
+                  src={`https://lvpr.tv?v=${assets.playbackId}`}&apos;
+                  <br />
+                  allow=&apos;autoplay; encrypted-media; picture-in-picture&apos;
+                  <br />
+                  sandbox=&apos;allow-scripts&apos;
+                  <br />
+                  {`</iframe>`}
+                </code>
+              </div>
+              {/* For CID */}
+              {assets.status.phase === 'ready' && assets.storage?.ipfs?.cid ? (
+                <div>
+                  <code>
+                    <p>Embed Player Code w/ CID</p>
+                    <br />
+                    {`<iframe>`}
+                    <br />
+                    src={`https://lvpr.tv?v=${assets.storage.ipfs.cid}`}&apos;
+                    <br />
+                    allow=&apos;autoplay; encrypted-media; picture-in-picture&apos;
+                    <br />
+                    sandbox=&apos;allow-scripts&apos;
+                    <br />
+                    {`</iframe>`}
+                  </code>
+                </div>
+              ) : null}
             </div>
           </div>
         ) : (
-          <Image src={logo} alt="Livepeer Studio Logo" width="256" height="256" />
+            <div style={ { display: 'flex',  justifyContent: 'center'}}>
+              <Image src={ logo } alt='Livepeer Studio Logo' width='256' height='256' layout='fixed' />
+              </div>
         )}
 
         {/* Display information about the asset */}
         <div className={styles.cardbody}>
           <h2> Name: {assets.name} </h2>
           <p>Status:</p>
-          {assets.status.phase === "ready" ? (
+          {assets.status.phase === 'ready' ? (
             <p className={styles.ready}>{assets.status.phase} </p>
           ) : (
             <p className={styles.failed}>{assets.status.phase}</p>
@@ -80,29 +101,35 @@ export default function AssetDetails({ assets }) {
           <p>Id:</p>
           {assets.id}
 
-          {assets.status.phase === "ready" ? null : (
+          {assets.status.phase === 'ready' ? null : (
             <div>
-              {" "}
-              <p>Error:</p> {assets.status.errorMessage}{" "}
+              {' '}
+              <p>Error:</p> {assets.status.errorMessage}{' '}
             </div>
           )}
-          {assets.status.phase === "ready" ? (
+          {assets.status.phase === 'ready' ? (
             <div>
               <p> Playback Id:</p>
               {assets.playbackId}
             </div>
           ) : null}
-          {assets.status.phase === "ready" ? (
+          {assets.status.phase === 'ready' ? (
             <div>
               <p>Playback URL</p> {assets.playbackUrl}
             </div>
           ) : null}
-          {assets.status.phase === "ready" ? (
+          {assets.status.phase === 'ready' ? (
             <div>
               <p>DownloadUrl</p>
-              <a className={styles.url} href={assets.downloadUrl} target="_blank" rel="noreferrer">
+              <a className={styles.url} href={assets.downloadUrl} target='_blank' rel='noreferrer'>
                 {assets.downloadUrl}
-              </a>{" "}
+              </a>{' '}
+            </div>
+          ) : null}
+          {assets.status.phase === 'ready' && assets.storage?.ipfs?.cid ? (
+            <div>
+              <p> CID:</p>
+              {assets.storage?.ipfs?.cid}
             </div>
           ) : null}
         </div>
