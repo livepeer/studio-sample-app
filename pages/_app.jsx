22e8934b5501f665b3bc884bafd6c54b4a276f06
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-10-19 13:53:30 -0500

    install dragzone and spinner dependencies/add walletconnect/add file drag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,34 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout';
 import { LivepeerConfig, createReactClient, studioProvider } from "@livepeer/react";
-
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+import { WagmiConfig, chain, createClient } from 'wagmi';
 
 
 function MyApp( { Component, pageProps } ) {
+
+  const wagmiClient = createClient(
+    getDefaultClient({
+      appName: 'livepeer.js',
+      chains: [ chain.polygonMumbai ],
+      infuraId: process.env.NEXT_PUBLIC_INFURA_API_KEY
+    })
+  )
+
    const client = createReactClient({
      provider: studioProvider({ apiKey: process.env.NEXT_PUBLIC_API_CORS }),
    });
   return (
     <>
-      <LivepeerConfig client={client}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </LivepeerConfig>
+      <WagmiConfig client={wagmiClient}>
+        <ConnectKitProvider>
+          <LivepeerConfig client={client}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </LivepeerConfig>
+        </ConnectKitProvider>
+      </WagmiConfig>
     </>
   );
 }
--- pages/mintNFT.tsx
+++ pages/mintNFT.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { useMemo, useCallback, useState } from 'react';
+import { useAsset, useUpdateAsset, useCreateAsset } from '@livepeer/react';
+import { ConnectKitButton } from 'connectkit';
+import { useDropzone } from 'react-dropzone';
+import BarLoader from 'react-spinners/Barloader';
+import { useAccount } from 'wagmi';
+import styles from '../styles/MintNFT.module.css';
 
-export default function mintNFT() {
+
+
+export default function MintNFT() {
+
+ const [video, setVideo] = useState<File | null>(null);
+
+  const { address } = useAccount();
+
+  const {
+    mutate: createAsset,
+    data: createdAsset,
+    status: createStatus,
+    uploadProgress,
+  } = useCreateAsset();
+
+  const { data: asset, status: assetStatus } = useAsset( {
+    assetId: createdAsset?.id,
+    refetchInterval: ( asset ) => ( asset?.storage?.status?.phase !== 'ready' ? 5000 : false ),
+  });
+
+  const { mutate: updateAsset, status: updateStatus } = useUpdateAsset();
+  
+const onDrop = useCallback(async (acceptedFiles: File[]) => {
+  if (acceptedFiles && acceptedFiles.length > 0 && acceptedFiles?.[0]) {
+    setVideo(acceptedFiles[0]);
+  }
+}, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'video/*': ['*.mp4'],
+    },
+    maxFiles: 1,
+    onDrop,
+  } );
+  
+
+    const progressFormatted = useMemo(
+      () =>
+        uploadProgress
+          ? `Uploading: ${Math.round(uploadProgress * 100)}%`
+          : asset?.status?.progress
+          ? `Processing: ${Math.round(asset?.status?.progress * 100)}%`
+          : null,
+      [uploadProgress, asset?.status?.progress]
+    );
+
   return (
-    <div>mintNFT</div>
-  )
+    <div>
+      <ConnectKitButton />
+      {address && (
+        <div>
+          <div className={styles.drop} {...getRootProps()}>
+            <input {...getInputProps()} />
+            <div>
+              <p>
+                Drag and drop or <span>browse files</span>
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.progress}>
+            {video ? <p>Name: {video.name}</p> : <p>Select a video file to upload.</p>}
+            {progressFormatted && <p>{progressFormatted}</p>}
+          </div>
+
+        </div>
+      )}
+    </div>
+  );
 }
